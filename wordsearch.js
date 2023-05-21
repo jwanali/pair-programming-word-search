71165dc696776e280e286fd231145b7dc469071a
jwanali <ciwan_ali@hotmail.com>
2023-05-21 10:16:58 -0500

    update

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+        if (l.includes(word)) {
+            return true
+         }
+     }
+    const vertical = transpose(horizontalJoin);
+     for (l of  vertical) {
+        if (l.includes(word)) {
+            return true
+         }
+     }
+     return false
+    
 
+}
+const transpose = function (matrix) {
+     let finalMatrix = [];
+    for (let y = 0; y < matrix[0].length; y++) {
+      let newArray = [];
+      for (let i = 0; i < matrix.length; i++) {
+        newArray.push(matrix[i][y]);
+      }
+      finalMatrix.push(newArray);
+    }
+    matrix = finalMatrix;
+    return matrix;
+  };
 module.exports = wordSearch
